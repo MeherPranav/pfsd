9b7df4864ea149382ac28d494dce236edd7c4e46
vishal <vishalkup02@gmail.com>
2022-02-25 21:15:05 +0530

    initial

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_heroku
 
@@ -126,6 +127,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATIC_URL = '/static/'
 MEDIA_URL = "/abcd/"
